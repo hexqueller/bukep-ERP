6f5f88c61f553c366213f526b92ea190909ac998
hexqueller <dima.nesed@gmail.com>
2025-01-07 12:38:59 +0300

    Create backend api app

--- Backend/main.go
+++ Backend/main.go
@@ -1,1 +1,22 @@
-package backend
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		currentTime := time.Now().Format("2006-01-02 15:04:05")
+		response := map[string]string{"time": currentTime}
+		json.NewEncoder(w).Encode(response)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
