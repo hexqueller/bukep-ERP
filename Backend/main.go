7286e40c73630e835ad57a50d2228b1e09c161fa
hexqueller <dima.nesed@gmail.com>
2025-01-20 17:30:46 +0300

    Remove useless endpoints

--- Backend/main.go
+++ Backend/main.go
@@ -4,21 +4,9 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	"time"
 )
 
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World!"))
-	})
-
-	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		currentTime := time.Now().Format("2006-01-02 15:04:05")
-		response := map[string]string{"time": currentTime}
-		json.NewEncoder(w).Encode(response)
-	})
-
 	http.HandleFunc("/up", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		response := map[string]string{"status": "up"}
